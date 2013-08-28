4d1ad4e3c8bb1036f0e924135b822545d08b9a32
Ahmad Syaltut <syaltut@cpan.org>
2013-08-28 13:23:07 +0000

    Import of SYALTUT/App-perldolicious-0.008 from CPAN.
    gitpan-cpan-distribution: App-perldolicious
    gitpan-cpan-version:      0.008
    gitpan-cpan-path:         SYALTUT/App-perldolicious-0.008.tar.gz
    gitpan-cpan-author:       SYALTUT
    gitpan-cpan-maturity:     released

--- lib/App/perldolicious.pm
+++ lib/App/perldolicious.pm
@@ -1,6 +1,6 @@
 package App::perldolicious;
 
-our $VERSION = '0.007';
+our $VERSION = '0.008';
 $VERSION = eval $VERSION;
 
 1;
@@ -13,7 +13,7 @@ App::perldolicious - Mojolicious::Lite webapp that lets you search (with regexp)
 
 =head1 VERSION
 
-Version 0.007.
+Version 0.008.
 
 =head1 DESCRIPTION
 
